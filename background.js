12426fb367c67ea1834c550d42fa2ee05d5a4c0e
Elise Rothberg <eliserothberg@gmail.com>
2024-01-28 22:48:28 -0800

    removed popup added notification

--- background.js
+++ background.js
@@ -1,29 +1,23 @@
-// Create the context menu item
 chrome.contextMenus.create({
     id: "saveWrangledImage",
     title: "Save Wrangled Image",
     contexts: ["image"]
 });
 
-// Add listener for context menu item clicks
+// Context menu item click listener
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "saveWrangledImage") {
         const imageUrl = info.srcUrl;
         const pageUrl = tab.url;
         const defaultFolderName = 'ImageWrangler';
-        let folderName = prompt("Enter the folder name to save the image:", defaultFolderName);
 
-        if (folderName === null) {
-            folderName = defaultFolderName; // Use default if prompt is cancelled
-        }
-
-        // Search for the specified folder or create it if it doesn't exist
-        chrome.bookmarks.search({ title: folderName }, (results) => {
+        // Search for the 'ImageWrangler' folder or create it if it doesn't exist
+        chrome.bookmarks.search({ title: defaultFolderName }, (results) => {
             let folderId = results.length > 0 ? results[0].id : null;
             if (!folderId) {
-                chrome.bookmarks.create({ title: folderName }, (newFolder) => {
+                chrome.bookmarks.create({ title: defaultFolderName }, (newFolder) => {
                     folderId = newFolder.id;
-                    createBookmark(folderId, pageUrl, imageUrl);
+                    createBookmark(folderId, pageUrl, imageUrl); // Image URL bookmark with page URL as title
                 });
             } else {
                 createBookmark(folderId, pageUrl, imageUrl);
@@ -32,13 +26,26 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
 });
 
-// Function to create a bookmark
 function createBookmark(folderId, title, url) {
     chrome.bookmarks.create({
         parentId: folderId,
         title: title,
         url: url
     }, (bookmark) => {
-        console.log('Bookmark created: ', bookmark);
+        if (bookmark) {
+            chrome.notifications.create({
+                type: 'basic',
+                iconUrl: 'images/icon48.png',
+                title: 'Bookmark Saved',
+                message: 'Your bookmark from ' + title + ' was saved successfully!'
+            });
+        } else {
+            chrome.notifications.create({
+                type: 'basic',
+                iconUrl: 'images/icon48.png',
+                title: 'Bookmark Failed',
+                message: 'Failed to save the bookmark from ' + title + '.'
+            });
+        }
     });
 }
